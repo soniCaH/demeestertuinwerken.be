f6e06f1da8d33141335d9623f8c7ab224e2c2ad7
Kevin Van Ransbeeck <kevin.van.ransbeeck@gmail.com>
2021-06-23 16:08:32 +0200

    Start styling header.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,9 @@
  */
 
 // You can delete this file if you're not using it
+
+exports.onRenderBody = ({ setHtmlAttributes }) => {
+  setHtmlAttributes({
+    className: "no-js",
+  })
+}
--- src/components/header.js
+++ src/components/header.js
@@ -3,19 +3,62 @@ import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
+import "./header.scss"
+
 const Header = ({ siteTitle }) => (
-  <header>
-    <div>
-      <h1>
-        <Link to="/">
-          <StaticImage
-            src="../images/logo-white.png"
-            alt={siteTitle}
-            height={50}
-            placeholder="tracedSVG"
-          />
-        </Link>
-      </h1>
+  <header data-sticky-container>
+    <div class="title-bar" data-responsive-toggle="navigation">
+      <Link to="/">
+        <StaticImage
+          src="../images/logo-greens.png"
+          alt={siteTitle}
+          formats={["auto", "webp", "avif"]}
+          placeholder="none"
+          height="50"
+        />
+      </Link>
+      <div className="menu-responsive">
+        <div class="title-bar-title">Menu</div>
+        <button
+          class="menu-icon"
+          type="button"
+          data-toggle="navigation"
+          aria-label="Toggle Responsive navigation"
+        ></button>
+      </div>
+    </div>
+    <div
+      class="top-bar"
+      id="navigation"
+      data-sticky
+      data-options="marginTop: 0;"
+    >
+      <Link to="/" className="hide-for-small-only">
+        <StaticImage
+          src="../images/logo-greens.png"
+          alt={siteTitle}
+          formats={["auto", "webp", "avif"]}
+          placeholder="none"
+          height="75"
+        />
+      </Link>
+      <ul class="menu">
+        <li>
+          <Link to="/" activeClassName="active" partiallyActive={true}>
+            One
+          </Link>
+        </li>
+        <li>
+          <Link to="/" activeClassName="active" partiallyActive={true}>
+            Two
+          </Link>
+        </li>
+        <li>
+          <Link to="/" activeClassName="active" partiallyActive={true}>
+            Three
+          </Link>
+        </li>
+      </ul>
     </div>
   </header>
 )
