4f03f03d6d707b584ee12585b6e5fdc83d9e29a4
Kevin Van Ransbeeck <kevin.van.ransbeeck@gmail.com>
2021-07-17 10:51:28 +0200

    Services overview page.

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,4 +1,6 @@
 import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../layouts/index"
 import Seo from "../components/seo"
@@ -8,8 +10,73 @@ const ServicesPage = () => (
     <Seo title="Onze services" />
     <div className="grid-container">
       <h1 className="border--after">Onze services</h1>
-
-      <p>Wij doen zowa vanalles hein!</p>
+      <div className="grid-x grid-margin-x grid-margin-y">
+        <Link to="/services/tuinaanleg" className="cell medium-6 grid-image">
+          <StaticImage
+            src="../images/home-foto1.jpg"
+            alt="Foto"
+            placeholder="blurred"
+          />
+          <h3 className="grid-image__title">Tuinaanleg</h3>
+        </Link>
+        <Link to="/services/tuinonderhoud" className="cell medium-6 grid-image">
+          <StaticImage
+            src="../images/home-foto1.jpg"
+            alt="Foto"
+            placeholder="blurred"
+          />
+          <h3 className="grid-image__title">Tuinonderhoud</h3>
+        </Link>
+        <Link to="/services/transport" className="cell medium-6 grid-image">
+          <StaticImage
+            src="../images/home-foto1.jpg"
+            alt="Foto"
+            placeholder="blurred"
+          />
+          <h3 className="grid-image__title">Transport</h3>
+        </Link>
+        <Link to="/services/realisaties" className="cell medium-6 grid-image">
+          <StaticImage
+            src="../images/home-foto1.jpg"
+            alt="Foto"
+            placeholder="blurred"
+          />
+          <h3 className="grid-image__title">Realisaties</h3>
+        </Link>
+        <Link to="/services/sportvelden" className="cell medium-6 grid-image">
+          <StaticImage
+            src="../images/home-foto1.jpg"
+            alt="Foto"
+            placeholder="blurred"
+          />
+          <h3 className="grid-image__title">Sportvelden</h3>
+        </Link>
+        <Link to="/services/grondwerken" className="cell medium-6 grid-image">
+          <StaticImage
+            src="../images/home-foto1.jpg"
+            alt="Foto"
+            placeholder="blurred"
+          />
+          <h3 className="grid-image__title">Grondwerken</h3>
+        </Link>
+        <Link to="/services/tuinpoorten" className="cell medium-6 grid-image">
+          <StaticImage src="../images/home-foto1.jpg" alt="Foto" />
+          <h3 className="grid-image__title">Tuinpoorten</h3>
+        </Link>
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem,
+        temporibus a reprehenderit, adipisci expedita nam odio neque alias
+        eligendi, consectetur sapiente dicta tenetur perspiciatis. Aspernatur
+        modi provident obcaecati impedit dignissimos. Lorem ipsum dolor sit
+        amet, consectetur adipisicing elit. Rem, temporibus a reprehenderit,
+        adipisci expedita nam odio neque alias eligendi, consectetur sapiente
+        dicta tenetur perspiciatis. Aspernatur modi provident obcaecati impedit
+        dignissimos. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Rem, temporibus a reprehenderit, adipisci expedita nam odio neque alias
+        eligendi, consectetur sapiente dicta tenetur perspiciatis. Aspernatur
+        modi provident obcaecati impedit dignissimos.
+      </p>
     </div>
   </Layout>
 )
