6ac3536a43abde0f2964c33b4f648b458ded5d08
Kevin Van Ransbeeck <kevin.van.ransbeeck@gmail.com>
2021-07-17 10:41:08 +0200

    About page text.

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,11 +5,35 @@ import Seo from "../components/seo"
 
 const AboutPage = () => (
   <Layout>
-    <Seo title="Over ons - Wie zijn wij" />
+    <Seo title="Over ons - Yannis Demeester" />
     <div className="grid-container">
-      <h1 className="border--after">Wie zijn wij</h1>
+      <h1 className="border--after">Wie zijn wij?</h1>
 
-      <p>Wij zijn de Yannis hein!@</p>
+      <h2 className="border--after">Yannis Demeester</h2>
+
+      <p>
+        Vanaf mijn 12 jaar volgde ik de opleiding "Tuinaanleg" in de
+        Tuinbouwschool Horteco in Vilvoorde. Na 7 jaar opleiding wist ik het
+        zeker: Dit het beroep voor mij...
+      </p>
+      <p>
+        Mijn eerste jaren heb ik ervaring opgedaan in een bedrijf
+        gespecialiseerd in de aanleg en het onderhoud van sportvelden. Samen met
+        de ervaring groeiden ook de kriebels om een eigen bedrijf op te starten.
+      </p>
+
+      <h2 className="border--after">Demeester Tuinwerken</h2>
+
+      <p>
+        In 2015 heb ik de stap gezet om mijn eigen bedrijf te starten en mijn
+        droom waar te maken.
+      </p>
+
+      <p>
+        @TODO: Aanvullen met foto's van camionetten, Tekstje hierboven wat
+        verder uitwerken, samenwerkingen wat beschrijven uitklaren "Wie zijn
+        wij" vs "Wie ben ik".
+      </p>
     </div>
   </Layout>
 )
