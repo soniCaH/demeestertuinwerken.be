9b1e2be941ef2fa57d6babebcce826d8170e2420
Kevin Van Ransbeeck <kevin.van.ransbeeck@gmail.com>
2022-03-12 14:04:57 +0100

    Update gatsby gradually. (#71)
    * Gatsby 4.7.1
    * Gatsby 4.8.0
    * Gatsby 4.9.0

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,6 @@ module.exports = {
         icon: `src/images/logo-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
